e407b9cc79ceb09435e956920bf4a295d362e0de
MichelDekiere <91069678+MichelDekiere@users.noreply.github.com>
2022-05-14 22:20:56 +0200

    using the sql server + insert 3 items and 4 players

---
+++ Items.sql
@@ -0,0 +1,22 @@
+USE [ActionCommandGame]
+GO
+
+INSERT INTO [dbo].[Item]
+           ([Name]
+           ,[Description]
+           ,[Price]
+           ,[Fuel]
+           ,[Attack]
+           ,[Defense]
+           ,[ActionCooldownSeconds])
+     VALUES
+           ('Screwdriver',
+		   '',
+		   50,
+		   0,
+		   50,
+		   0,
+		   0)
+GO
+
+
---
+++ Players.sql
@@ -0,0 +1,14 @@
+USE [ActionCommandGame]
+GO
+
+INSERT INTO [dbo].[Player]
+           ([Name]
+           ,[Money]
+           ,[Experience]
+           ,[UserId])
+     VALUES
+           ('John Doe'
+           , 100
+           , 0
+           ,'22227bfc-9fd5-4484-9596-3e31ebf562b3')
+GO
